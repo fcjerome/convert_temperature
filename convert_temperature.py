ce5c9f71811a61ba85424a328b5a4d5cec21f34f
fcjerome <j.franciscoconceicao@tudelft.nl>
2023-05-25 14:40:22 +0200

    docstring for source code documentation

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,3 +1,12 @@
 def Farenheit_to_celcius(temp_farenheit):
+	"""
+	Converte temperature in farenheit to celcius
+	Parameters:
+	temp_farenheit
+
+	return:
+	temp_celcius
+	"""
+
 	temp_celcius = (temp_farenheit -32)*(5.0/9.0)
 	return temp_celcius
